ab1fffc69a77bba26797aae4ad0d2293bb039af5
Matthew Curran <qe4452ur@t3040-2-5.local>
2019-03-19 18:30:37 -0500

    Created ResourceDataController class.

--- ProgrammerResources/Resource.swift
+++ ProgrammerResources/Resource.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct Resource {
+    let name: String
+    let description: String
+    let url: URL
+}
+
+extension Resource {
+    init (dictionary: [String: String]) {
+        name = dictionary["name"]! as String
+        description = dictionary["description"]! as String
+        let urlString = dictionary["url"]! as String
+        url = URL(string: urlString)!
+    }
+}
